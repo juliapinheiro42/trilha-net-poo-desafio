0da391344c594d22293306a088ddad83373155c3
Julia Pinheiro de Farias <juliapinheiro.142@gmail.com>
2025-06-05 11:14:05 -0300

    desafio instalar aplicativo no celular POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111222333", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Smartphone iphone = new Iphone("654321", "iPhone 13", "444555666", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
